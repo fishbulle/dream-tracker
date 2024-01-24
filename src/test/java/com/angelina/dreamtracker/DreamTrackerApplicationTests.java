da8e8b6667b72cc6ddcd2ed4fbd8e1d0afc57450
amam <angelina.malmros@sbab.se>
2024-01-24 09:05:06 +0100

    Initial setup

---
+++ src/test/java/com/angelina/dreamtracker/DreamTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelina.dreamtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
