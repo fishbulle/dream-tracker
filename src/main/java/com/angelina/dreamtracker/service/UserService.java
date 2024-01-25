15deda6f47671cd689055dcfbf9b4473d13eb53f
amam <angelina.malmros@sbab.se>
2024-01-25 08:55:56 +0100

    WIP UserService

---
+++ src/main/java/com/angelina/dreamtracker/service/UserService.java
@@ -0,0 +1,14 @@
+package com.angelina.dreamtracker.service;
+
+import com.angelina.dreamtracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+}
