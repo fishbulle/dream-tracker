285afecb846d0f62c70b1c93212c1a205600ab6c
amam <angelina.malmros@sbab.se>
2024-01-25 09:34:38 +0100

    GenerationType for ID

--- src/main/java/com/angelina/dreamtracker/model/Dream.java
+++ src/main/java/com/angelina/dreamtracker/model/Dream.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 public class Dream {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String title;
     private String content;
