1ed37ad11652a9f1b905f0f7eb58558a44d32069
amam <angelina.malmros@sbab.se>
2024-02-19 16:42:51 +0100

    Added cors config

---
+++ src/main/java/com/angelina/dreamtracker/security/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.angelina.dreamtracker.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://localhost:5173",
+                        "https://localhost:3000",
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "https://www.localhost:5173",
+                        "https://www.localhost:3000",
+                        "http://www.localhost:5173",
+                        "http://www.localhost:3000"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
